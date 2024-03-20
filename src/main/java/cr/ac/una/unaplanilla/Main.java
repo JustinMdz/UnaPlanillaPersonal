ddc97158320d53c729be0c1f075359c3bd57a114
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-03-19 20:21:27 -0600

    pom updates

---
+++ src/main/java/cr/ac/una/unaplanilla/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.unaplanilla;
+
+/**
+ *
+ * @author justi
+ */
+public class Main {
+   public static void main(String[] args) {
+        App.main(args);
+    } 
+}
